aa2466635136327800de76db43cf1cddafbd4daa
Timur Sanatov <neizpro@gmail.com>
2026-01-17 22:57:49 +0300

    Работа завершена

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import { initTable } from "./components/table.js";
 import { initPagination } from "./components/pagination.js";
 import { initSorting } from "./components/sorting.js";
 import { initFiltering } from "./components/filtering.js";
+import { initSearching } from "./components/searching.js";
 // @todo: подключение
 
 // Исходные данные используемые в render()
@@ -42,7 +43,7 @@ function render(action) {
   // @todo: использование
   result = applyPagination(result, state, action);
   result = applySorting(result, state, action);
-  result = 
+  result = applySearching(result, state, action);
   result = applyFiltering(result, state, action);
 
   sampleTable.render(result);
@@ -77,7 +78,7 @@ const applySorting = initSorting([
   sampleTable.header.elements.sortByTotal,
 ]);
 
-const applySearching = initinitSearching(''); "TODO"
+const applySearching = initSearching('search');
 
 const applyFiltering = initFiltering(sampleTable.filter.elements, {
   // передаём элементы фильтра
